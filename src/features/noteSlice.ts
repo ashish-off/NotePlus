995528465d2286052472f5fd0e3bf076ecf44e8e
Ashish <ashishsaud498@gmail.com>
2025-02-10 23:39:41 +0545

    react redux toolkit setup done

--- src/features/noteSlice.ts
+++ src/features/noteSlice.ts
@@ -6,17 +6,17 @@ const noteSlice = createSlice({
     notes: [],
   },
   reducers: {
-    addNote: (state, action) => {
-      state.notes.push(action.payload);
+    addNote: (state, action) => { // takes a object
+      state.notes.push(action.payload); 
     },
-    editNote: (state, action) => {
+    editNote: (state, action) => { // takes a id and edited object
       const { id, editedNote } = action.payload;
-      const index = state.notes.findIndex((note) => note.id === id);
+      const index : number = state.notes.findIndex((note) => note.id === id);
       if (index!== -1) {
         state.notes[index] = editedNote;
       }
     },
-    deleteNote: (state, action) => {
+    deleteNote: (state, action) => { // takes a id
       const { id } = action.payload;
       const index = state.notes.findIndex((note) => note.id === id);
       if (index!== -1) {
